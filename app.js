cbb6ccd8910be243f6d8f01d8d820196a1d19579
Perry Huang <perryhuang2015@gmail.com>
2025-08-27 15:27:08 -0400

    feat: organize app and add functionality

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require("express");
-const { body, validationResult } = require("express-validator");
 const path = require("node:path");
 const indexRouter = require("./routes/indexRouter");
+const folderRouter = require("./routes/folderRouter");
+const passport = require("passport");
+const session = require("./config/sessionConfig");
 
 const app = express();
-app.use(express.urlencoded({ extended: true })); // make post work and have a body
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 const assetsPath = path.join(__dirname, "public");
+app.use(express.urlencoded({ extended: true })); // make post work and have a body
 app.use(express.static(assetsPath));
 
+app.use(session);
+app.use(passport.session());
+require("./config/passportConfig");
+
+app.use("/folder", folderRouter);
 app.use("/", indexRouter);
 
 const PORT = 3000;
