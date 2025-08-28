24df72aa3ac5e4a46ecb51ee8692e739d7464915
Perry Huang <perryhuang2015@gmail.com>
2025-08-27 21:40:05 -0400

    feat: partially add folderId and viewing folders

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -1,7 +1,22 @@
 const db = require("../db/queries"); // require db later
 
 const getFolder = async (req, res) => {
-    res.render("folder", { title: "Folder", folderId: 0 }); // render the user's default folderid as well
+    if (req.isAuthenticated()) {
+        let folder;
+        if (req.params.folderId) {
+            folder = await db.getFolderById(Number(req.params.folderId));
+        } else {
+            folder = await db.getFolderById(req.user.folderId);
+        }
+        if (folder.userId !== req.user.id) {
+            return res.render("folder", {
+                title: "You do not have access to this folder!",
+                folder: null,
+            });
+        }
+        return res.render("folder", { title: folder.name, folder: folder });
+    }
+    res.render("folder", { title: "Folder" });
 };
 
 const createFolder = async (req, res) => {
--- routes/folderRouter.js
+++ routes/folderRouter.js
@@ -5,6 +5,7 @@ const { Router } = require("express");
 const folderRouter = Router();
 
 folderRouter.get("/", folderController.getFolder);
+folderRouter.get("/:folderid", folderController.getFolder);
 folderRouter.post("/", folderController.createFolder);
 folderRouter.post("/file", folderController.uploadFile);
 
