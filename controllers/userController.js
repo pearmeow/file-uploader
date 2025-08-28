fd3ce6509aacc09d479f5f4eaa0bdda5b05fa821
Perry Huang <perryhuang2015@gmail.com>
2025-08-27 20:38:14 -0400

    feat: make login redirect back to index

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,7 +37,7 @@ const postRegister = [
 const postLogin = passport.authenticate("local", {
     failureMessage: "Username or password wrong",
     failureRedirect: "/",
-    successRedirect: "/folder",
+    successRedirect: "/",
 });
 
 const getLogout = (req, res, next) => {
